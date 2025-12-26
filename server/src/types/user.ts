19e4ec616ba1c2fd453d6d455f90f5091212114c
Tony Hsu Tai <hsutai.c@gmail.com>
2025-12-25 19:09:49 -0500

    added data types for endpoints

--- server/src/types/user.ts
+++ server/src/types/user.ts
@@ -4,3 +4,47 @@ export type UserProfile = {
   followers: { total: number };
   images: { url: string };
 }
+
+export type topArtist = {
+  items: Array<{
+    id: string;
+    followers: { total: number };
+    name: string;
+    images: Array<{ url: string }>;
+  }>
+}
+
+export type topTrack = {
+  items: Array<{
+    id: string;
+    name: string;
+    duration_ms: number;
+    album: {
+      id: string;
+      name: string;
+      images: Array<{ url: string }>;
+    };
+    artists: Array<{
+      id: string;
+      name: string;
+    }>;
+  }>;
+};
+
+export type recentTracks = {
+  items: Array<{
+    track: {
+      album: { 
+        name: string;
+        images: Array<{ url: string }>;
+      },
+      artists: Array<{
+        id: string;
+        name: string;
+      }>,
+      duration_ms: number;
+      id: string;
+      name: string;
+    }
+  }>;
+};
